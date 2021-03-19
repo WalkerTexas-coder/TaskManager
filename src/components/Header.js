b4748c3c926b1353efa4d48a9bc5a05a403785fe
Austin Walker <austinsrwalker@gmail.com>
2021-03-18 19:24:49 -0500

    router added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types'
 import Button from'./Button.js'
+import { useLocation} from 'react-router-dom'
 
 const Header = ({ title, onAdd, showAddTask }) => {
-    
+    const location = useLocation()
+
     return (
         <header className='header'>
            <h1 >{title}</h1>
-           <Button color={showAddTask ? 'Darkred' : 'Green'} text={showAddTask ? 'Close' : 'Add'} onClick = {onAdd}/>
+           {location.pathname === '/' && (<Button color={showAddTask ? 'Darkred' : 'Green'} text={showAddTask ? 'Close' : 'Add'} onClick = {onAdd}/>)}
         </header>
     )
 }
